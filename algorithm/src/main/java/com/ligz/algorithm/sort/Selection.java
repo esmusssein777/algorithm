04a131448ca521b898099f975e9041a9b114ebc8
ligz <abc.123456>
2018-12-18 18:38:58 +0800

    选择排序

---
+++ algorithm/src/main/java/com/ligz/algorithm/sort/Selection.java
@@ -0,0 +1,32 @@
+package com.ligz.algorithm.sort;
+
+/**
+ * 选择排序
+ * 找出最小/最大，放置，重复
+ * @author ligz
+ */
+public class Selection implements Sort{
+
+	public int[] sort(int[] array) {
+		int length = array.length;
+		//N-1轮
+		for(int i = 0; i < length-1; i++) {
+			int min = i;
+			//N-i轮
+			for(int j = i + 1; j < length; j++) {
+				//使用下标来记录数组的数据
+				if(array[min] > array[j]) {
+					min = j;
+				}
+			}
+			
+			if(min != i) {
+				int temp = array[i];
+				array[i] = array[min];
+				array[min] = temp;
+			}
+		}
+		return array;
+	}
+
+}
