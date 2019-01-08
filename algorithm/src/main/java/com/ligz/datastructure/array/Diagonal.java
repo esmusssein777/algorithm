2fbe99a85fe53b980a138b8c81972bd9f09a69e6
ligz <abc.123456>
2019-01-08 11:37:49 +0800

    对角线遍历

--- algorithm/src/main/java/com/ligz/datastructure/array/Diagonal.java
+++ algorithm/src/main/java/com/ligz/datastructure/array/Diagonal.java
@@ -6,29 +6,37 @@ package com.ligz.datastructure.array;
  */
 public class Diagonal {
 	public int[] findDiagonalOrder(int[][] matrix) {
-		boolean rightOrButton = true;//判断是向右，还是向下,true为右，false为下
+		if(matrix == null || matrix.length == 0) {
+			return new int[0];
+		}
 		int[] na = new int[matrix.length * matrix[0].length];
 		na[0] = matrix[0][0];
-		int index = 1;
+		int index = 0;
 		int i = 0;
 		int j = 0;
-		while(i != matrix.length - 1 && j != matrix[0].length - 1) {
-			if(rightOrButton && j < matrix[0].length) {//向右，且右边不为0
-				j++;
-				rightOrButton = false;
-			}else if(!rightOrButton && i < matrix.length){//向下，且下边不为0
-				i++;
-				rightOrButton = true;
-			}else if(rightOrButton) {
-				j++;
-				i--;
-			}else {
-				j--;
-				i++;
+		while(index != matrix.length * matrix[0].length - 1) {
+			if((i + j) % 2 == 0) {//横纵+为偶数，只能向右，向下，或者右上
+				if(i == 0 && j < matrix[0].length - 1) {//向右
+					j++;
+				}else if(j == matrix[0].length - 1) {//向下
+					i++;
+				}else {//右上
+					i--;
+					j++;
+				}
+			}else {//奇数，向下，向右，向左上
+				if(j == 0 && i < matrix.length - 1) {
+					i++;
+				}else if(i == matrix.length - 1){
+					j++;
+				}else {
+					i++;
+					j--;
+				}
 			}
-			na[index] = matrix[i][j];
 			index++;
+			na[index] = matrix[i][j];
 		}
 		return na;
-    }	
+    }
 }
