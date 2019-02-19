4721b217aafa6e4a9cf94ec38aa1af46411ecc8f
esmusssein777 <380449395@qq.com>
2019-02-19 17:16:46 +0800

    二叉搜索树

--- algorithm/src/main/java/com/ligz/algorithm/binarysearch/FindPeakElement.java
+++ algorithm/src/main/java/com/ligz/algorithm/binarysearch/FindPeakElement.java
@@ -10,13 +10,18 @@ package com.ligz.algorithm.binarysearch;
  */
 public class FindPeakElement {
     public int findPeakElement(int[] nums) {
-        int left = 1, right = nums.length - 2;
-        while (left < right){
-            int mid = left + (right - left) / 2;
-            if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid;
-
-
+        int left = 0, right = nums.length - 1;
+        if(nums.length == 1){
+            return 0;
         }
-        return -1;
+        while (left + 1 < right){
+            int mid = (left + right) / 2;
+            if (nums[mid] < nums[mid + 1]){
+                left = mid + 1;
+            }else {
+                right = mid;
+            }
+        }
+        return (left == nums.length - 1 || nums[left] > nums[left + 1]) ? left : right;
     }
 }
---
+++ algorithm/src/main/java/com/ligz/algorithm/binarysearch/SearchRange.java
@@ -0,0 +1,48 @@
+package com.ligz.algorithm.binarysearch;
+
+/**
+ * 在排序数组中查找元素的第一个和最后一个位置
+ * author:ligz
+ * 输入: nums = [5,7,7,8,8,10], target = 8
+ * 输出: [3,4]
+ */
+public class SearchRange {
+    public int[] searchRange(int[] nums, int target) {
+        int[] result = new int[2];
+        result[0] = findFirst(nums, target);
+        result[1] = findLast(nums, target);
+        return result;
+    }
+
+    private int findFirst(int[] nums, int target){
+        int idx = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if(nums[mid] >= target){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+            if(nums[mid] == target) idx = mid;
+        }
+        return idx;
+    }
+
+    private int findLast(int[] nums, int target){
+        int idx = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if(nums[mid] <= target){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+            if(nums[mid] == target) idx = mid;
+        }
+        return idx;
+    }
+}
