a803e5690c65c93b71cef8b554213217f74cd80a
esmusssein777 <380449395@qq.com>
2019-03-20 15:55:26 +0800

    算法更新

---
+++ algorithm/src/main/java/com/ligz/datastructure/list/RandomListNode.java
@@ -0,0 +1,15 @@
+package com.ligz.datastructure.list;
+
+/**
+ * 复杂链表
+ * author:ligz
+ */
+public class RandomListNode {
+    public int label;
+    public RandomListNode next = null;
+    public RandomListNode random = null;
+
+    public RandomListNode(int label) {
+        this.label = label;
+    }
+}
